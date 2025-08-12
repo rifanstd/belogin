500a1fdbf39e6ca369f5a2cc430ca4ddba77afbc
Rifan Setiadi <rfan2442@gmail.com>
2025-08-12 10:33:47 +0700

    feat: add https module and setup SSL configuration

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import helmet from "helmet";
 import cors from "cors";
 import router from "./routes/routes";
 import bodyParser from "body-parser";
+import fs from "fs";
+import https from "https";
+import path from "path";
 
 // Load environment variables from .env file
 dotenv.config();
@@ -11,6 +14,12 @@ dotenv.config();
 // Create an Express application
 const app: Application = express();
 const PORT = Number(process.env.PORT);
+
+// Setup SSL
+// const options = {
+//   key: fs.readFileSync(path.join(__dirname, "../localhost.key")),
+//   cert: fs.readFileSync(path.join(__dirname, "../localhost.crt")),
+// };
 
 // Middlewares
 app.use(helmet()); // Security headers
@@ -22,6 +31,9 @@ app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use(router); // Use the routes defined in routes.ts
 
 // Start the server
+// https.createServer(options, app).listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
